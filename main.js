bcb4c06f27bdffc1ada239a9e83f392b30a95932
Garrett Bryant <goofy70geb@gmail.com>
2016-03-28 11:45:15 -0400

    Initial commit
    Base commit with a starter Angular Material website.
    The starter site has been modified to use a simple project structure

---
+++ main.js
@@ -0,0 +1,20 @@
+(function(){
+    'use strict';
+
+    var myApp = angular.module('myApp', ['ngMaterial', 'users']);
+
+    myApp.config(function($mdThemingProvider, $mdIconProvider){
+      $mdIconProvider
+          .defaultIconSet("./images/avatars.svg", 128)
+          .icon("menu"       , "./images/menu.svg"        , 24)
+          .icon("share"      , "./images/share.svg"       , 24)
+          .icon("google_plus", "./images/google_plus.svg" , 512)
+          .icon("hangouts"   , "./images/hangouts.svg"    , 512)
+          .icon("twitter"    , "./images/twitter.svg"     , 512)
+          .icon("phone"      , "./images/phone.svg"       , 512);
+          $mdThemingProvider.theme('default')
+              .primaryPalette('brown')
+              .accentPalette('red');
+    });
+
+})();
